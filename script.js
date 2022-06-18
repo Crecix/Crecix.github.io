cd49edc3efa5aff1d7c56073d3bf6102dc4cb9ac
Crecix <105209429+Crecix@users.noreply.github.com>
2022-06-18 19:58:34 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var email = document.getElementById("mail");
-email.addEventListener("keyup", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("Looks like this is not an email");
-  } else {
-    email.setCustomValidity("");
-  }
-});
